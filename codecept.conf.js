0e75a15a6fb336dffa28133507c51f391148da5e
Thomas Tupper <ttupper@uxdesignassociates.com>
2021-08-26 16:22:18 -0500

    initial

---
+++ codecept.conf.js
@@ -0,0 +1,35 @@
+exports.config = {
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://www.saucedemo.com/',
+      show: true,
+      windowSize: '1200x600'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    }
+  },
+  tests: './*_test.js',
+  name: 'codeceptjs-web-POC'
+}
